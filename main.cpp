026b029ef66f1234025e9394d04cb058e92095c3
Pavel-Gornov <77741438+Pavel-Gornov@users.noreply.github.com>
2024-05-03 20:42:30 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main() {
+    int n;
+    std::cin >> n;
+    for (int i = 0; i < n; i++) {
+        for (int k = 0; k < (n - i); k++) {
+            std::cout << " ";
+        }
+        for (int j = 0; j < (i * 2 + 1); j++) {
+            std::cout << "*";
+        }
+        std::cout << std::endl;
+    }
+}
